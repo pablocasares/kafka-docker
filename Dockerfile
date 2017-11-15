bbfc81d6c597f08f83c31ed50c458f4a2e445155
Pablo Casares <lito92vforce@gmail.com>
2017-11-15 13:20:58 +0100

    WIZZIE-399 Remove AutoExpand from kafka-docker (#3)

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,6 @@ MAINTAINER wizzie.io
 
 RUN java -version
 
-ADD . /tmp/build
-RUN cd /tmp/build && \
-    ./gradlew -Dorg.gradle.native=false build && \
-    cp build/libs/kubernetes-expander-1.0-SNAPSHOT.jar $KAFKA_HOME/libs/
-
-ADD kafka-autoextend-partitions.sh /usr/bin/kafka-autoextend-partitions.sh
 ADD kafka-kubernetes-start.sh /usr/bin/kafka-kubernetes-start.sh
 ADD kafkaOk.sh /usr/bin/kafkaOk.sh
 
--- kafka-kubernetes-start.sh
+++ kafka-kubernetes-start.sh
@@ -19,23 +19,8 @@ fi
 
 
 if [[ -z "$KAFKA_BROKER_ID" ]]; then
-	if [[ -z "$KUBERNETS_UID" ]]; then
-		export KUBERNETS_UID=$HOSTNAME
-	fi
-    echo "Generate Kafka Broker ID: for $KUBERNETS_UID"
-	ID=`$KAFKA_HOME/bin/kafka-run-class.sh kafka.admin.AutoExpandCommand --zookeeper=$KAFKA_ZOOKEEPER_CONNECT -broker=$KUBERNETS_UID -mode=generate`
-	if [[ -z "$ID" ]]; then
-		echo "Got empty broker ID from kafka.admin.AutoExpandCommand; not starting!"
-		exit 1
-	fi
-	echo "Use broker ID: $ID"
-	export KAFKA_BROKER_ID=$ID
+	echo "Got empty broker ID from hostname; not starting!"
+	exit 1
 fi
 
-if [[ -n "$ENABLE_AUTO_EXTEND" ]]; then
-	echo "Enable auto exand"
-	/usr/bin/kafka-autoextend-partitions.sh &
-	/usr/bin/start-kafka.sh
-else
-	/usr/bin/start-kafka.sh
-fi
+/usr/bin/start-kafka.sh
